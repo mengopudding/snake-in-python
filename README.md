0b20a504edc95741499b534ede128d246b18648f
Meng Lim <meng@myxplor.com>
2019-05-26 15:10:04 +1000

    set initial position for snake and the snake's food

--- README.md
+++ README.md
@@ -7,7 +7,9 @@ My intro to Python with a childhood memory
 **Random**
 
 - Module for generating pseudo-random Numbers
+https://docs.python.org/3.7/library/random.html
 
 **Curses**
   
   - Handle keyboard input and control the displayed text on the terminal
+  https://docs.python.org/3/howto/curses.html
--- snake_game.py
+++ snake_game.py
@@ -1,2 +1,20 @@
 import random
 import curses
+
+stdscr = curses.initscr()
+curses.curs_set(0)
+screen_height, screen_width = stdscr.getmaxyx()
+window = curses.newwin(screen_height, screen_width, 0 ,0)
+window.keypad(1)
+window.timeout(100)
+
+snake_x = screen_width/4
+snake_y = screen_height/2
+snake = [
+    [snake_y, snake_x],
+    [snake_y, snake_x-1],
+    [snake_y, snake_x-2],
+]
+
+food = [screen_height/2, screen_width/2]
+window.addch(food[0], food[1], curses.ACS_BLOCK)
